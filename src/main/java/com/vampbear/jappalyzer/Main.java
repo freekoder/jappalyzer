ca85342174cd55a5682bc43f65afbe121472bf36
freekoder <freekoder@yandex.ru>
2022-03-24 23:36:43 +0300

    add grouping by categories at console app

--- src/main/java/com/vampbear/jappalyzer/Main.java
+++ src/main/java/com/vampbear/jappalyzer/Main.java
@@ -3,20 +3,21 @@ package com.vampbear.jappalyzer;
 import org.apache.commons.cli.*;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
 
     private static boolean verbose = false;
     private static boolean latest = false;
+    private static boolean groupByCategories = false;
 
     public static void main(String[] args) {
         long startTimestamp = System.currentTimeMillis();
         final Options options = new Options();
         options.addOption("v", "verbose", false, "add verbose information to output");
         options.addOption("l", "latest", false, "load latest technologies data");
+        options.addOption("c", "categories", false, "group by categories");
         CommandLineParser argsParser = new DefaultParser();
         try {
             CommandLine cmd = argsParser.parse(options, args);
@@ -25,6 +26,9 @@ public class Main {
             }
             if (cmd.hasOption("latest")) {
                 latest = true;
+            }
+            if (cmd.hasOption("categories")) {
+                groupByCategories = true;
             }
             List<String> argList = cmd.getArgList();
             for (String arg : argList) {
@@ -59,25 +63,45 @@ public class Main {
             if (verbose) System.out.println("Page loaded with " + duration + "ms");
             Set<TechnologyMatch> foundTechs = jappalyzer.fromPageResponse(pageResponse);
             System.out.println("Technologies:");
-            for (TechnologyMatch match : foundTechs) {
-                System.out.println(getTechnologyMatchString(match));
+            if (groupByCategories) {
+                Map<Category, List<TechnologyMatch>> categoryMap = new HashMap<>();
+                for (TechnologyMatch match : foundTechs) {
+                    List<Category> categories = match.getTechnology().getCategories();
+                    for (Category category : categories) {
+                        categoryMap.putIfAbsent(category, new LinkedList<>());
+                        categoryMap.get(category).add(match);
+                    }
+                }
+                for (Category category : categoryMap.keySet()) {
+                    System.out.println("[" + category.getName() + "]:");
+                    for (TechnologyMatch match : categoryMap.get(category)) {
+                        System.out.println(getTechnologyMatchString(match, false, true));
+                    }
+                }
+            } else {
+                for (TechnologyMatch match : foundTechs) {
+                    System.out.println(getTechnologyMatchString(match, true, false));
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String getTechnologyMatchString(TechnologyMatch technologyMatch) {
+    private static String getTechnologyMatchString(TechnologyMatch technologyMatch, boolean showCategory, boolean padding) {
         StringBuilder sb = new StringBuilder();
+        if (padding) sb.append("  ");
         sb.append("* ");
         sb.append(technologyMatch.getTechnology().getName());
         if (!technologyMatch.getVersion().isEmpty()) {
             sb.append(" (").append(technologyMatch.getVersion()).append(")");
         }
-        List<String> categoriesNames = technologyMatch.getTechnology().getCategories().stream()
-                .map(Category::getName)
-                .collect(Collectors.toList());
-        sb.append(" [").append(String.join(", ", categoriesNames)).append("]");
+        if (showCategory) {
+            List<String> categoriesNames = technologyMatch.getTechnology().getCategories().stream()
+                    .map(Category::getName)
+                    .collect(Collectors.toList());
+            sb.append(" [").append(String.join(", ", categoriesNames)).append("]");
+        }
         return sb.toString();
     }
 }
